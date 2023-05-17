fe867e55d5f5faa7429a9eae3669bda7bd56aeca
AbmaelFerreira <abmael_ninha@hotmail.com>
2023-05-17 14:52:58 -0400

    feat(Query: Alterando uma Entity-Migrations-Doctrine)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\News;
 use App\Service\NewsService;
 use App\service\StringManipulationService;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -16,13 +18,7 @@ use function Symfony\Component\String\u;
 
 class HomeController extends  AbstractController {
 
-
-    public function __construct(
-        #[Autowire('kernel.debug')]
-            private bool $isDebug
-    )
-    {
-    }
+    public function __construct( #[Autowire('kernel.debug')] private bool $isDebug ) { }
 
     #[Route('/', name: 'app_home')]
     public function home(
@@ -40,90 +36,32 @@ class HomeController extends  AbstractController {
 
         $response =$httpclient->request('GET','https://127.0.0.1:8000/api/news/12');
 
-                $logger->warning('Acessou a home');
+        $logger->warning('Acessou a home');
 
-                $logger->error('Array criada');
+        $logger->error('Array criada');
 
-                $pageTitle =  "Sistama de Notícias";
+        $pageTitle =  "Sistama de Notícias";
 
-                $logger->info('Titulo definido');
+        $logger->info('Titulo definido');
 
-//                $html = $twig->render('hello/homepage.html.twig', [
-//
-//                    'pageTitle' => $pageTitle,
-//                    'categories' => $categories,
-//                ]);
+        return $this->render('hello/homepage.html.twig',[
 
-
-               // return new Response($html);
-              return $this->render('hello/homepage.html.twig',[
-
-                   'pageTitle' => $pageTitle,
-                  'categories' => $service->getCategoryList($httpClient),
-//                   'categories' => $categories,
-                  ]);
-
+             'pageTitle' => $pageTitle,
+             'categories' => $service->getCategoryList($httpClient),
+            ]);
     }
 
     #[Route('/categoria/{slug}', name: 'app_category')]
-    public function category(
-                                string $slug=null,
-                                HttpClientInterface $httpClient,
-                                NewsService $service):Response{
+    public function category( $slug,EntityManagerInterface $entityManager ):Response{
 
-
+        $newsRepository = $entityManager->getRepository(News::class);
+        $news = $newsRepository->findAll();
         $pageTitle =  $slug;
-
 
         return $this->render('hello/category.html.twig', [
 
-            'categories' => $service->getCategoryList($httpClient),
             'pageTitle' => $pageTitle,
-            'news' => $service->getNewsList($httpClient)
+            'news'=> $news
        ]);
     }
-
-
-    #[Route('/reportagens')]
-    public function getTest(
-                            Request $request,
-                            HttpClientInterface $httpClient,
-                            NewsService $service): Response {
-
-                $nome = "";
-                $sobrenome = "";
-
-                if($request->get('nome')){
-                    $nome =  $request->get('nome');
-                }
-                if($request->get('sobrenome')){
-                    $sobrenome =  $request->get('sobrenome');
-                }
-                dump($nome);
-                dump($sobrenome);
-
-            return $this->render('reportagens.html.twig', [
-            'categories' => $service->getCategoryList($httpClient),
-            //'nome' => $nome,
-            //'sobrenome' => $sobrenome
-        ]);
-    }
-
-
-
-
-//    #[Route('/news/{id}')]
-//    public function newsDetail(int $id=null, HttpClientInterface $httpclient){
-//            $response =$httpclient->request('GET','https://127.0.0.1:8000/api/news/'.$id);
-//            dd($response->toArray());
-//            exit;
-//    }
-
-//        #[Route('/animal/{slug}')]
-//        public function animal(string $slug=null):Response{
-//
-//            $newSlug = str_replace('-', ' ',$slug);
-//            $newSlug = u($newSlug)->title(true);
-//            return new Response('Olá : '.$newSlug);
-//        }
 }
